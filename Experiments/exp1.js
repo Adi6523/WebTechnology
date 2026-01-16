e466e04e939986f4fd1b3db4ca6ecf259265dbfd
Aditya Patil <adityap192005@gmail.com>
2026-01-16 12:11:19 +0530

    Exp 1

---
+++ Experiments/exp1.js
@@ -0,0 +1,94 @@
+//Activity 1
+/*
+const account_id =100;
+account_id =200;
+console.log(account_id);
+*/
+
+//Activity 2
+/*
+let a= 10;
+var b= 20;
+console.log(a,b);
+{
+    let a= 100;
+    var b= 200;
+    console.log(a,b);
+}
+console.log(a,b);
+*/
+
+//Activity 3
+/*
+let id=5;
+let accountno=5;
+let accname="Aditya";
+let status=true;
+let bigint=BigInt(9);
+let nullval=null;
+let undefined;
+let symbol=Symbol(id);
+
+console.log(accountno,accname,status,bigint,nullval,undefined,symbol);
+console.log(typeof(id));
+console.log(typeof(accountno));
+console.log(typeof(accname));
+console.log(typeof(status));
+console.log(typeof(bigint));
+console.log(typeof(nullval));
+console.log(typeof(undefined));
+console.log(typeof(symbol));
+*/
+
+//Activity 5 Student info print
+/*
+let rollno=90;
+let name="Aditya Patil";
+let email="adityap192005@gmail.com"
+
+console.table([rollno,name,email]);
+*/
+
+//Activity 6 Even odd
+/*
+let x=6,y=5;
+
+if(x%2==0)
+{console.log("Even No.");}
+else
+{console.log("Odd No.");}
+*/
+
+//Activity 7 loop print 1 to 10
+/*
+for(i=1;i<=10;i++)
+{
+    console.log(i);
+}
+*/
+
+//Activity 8 Pass or Fail
+/*
+Marks = 56;
+if(Marks>=40)
+{
+    console.log("Pass");
+}
+else
+{
+    console.log("Fail");
+}
+*/
+
+
+//Acitvity 9 log
+/*
+let x=10
+y=x;
+console.log(x,y);
+x=100;
+console.log(x,y);
+*/
+
+//Activity 10 create html file and 
+console.log("Aditya");
